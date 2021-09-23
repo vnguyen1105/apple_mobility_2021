36531eb735b23e4f501080e27c0a980b9c42f88d
Vanessa Nguyen <vtnguyen27@dons.usfca.edu>
2021-09-23 04:00:01 -0700

    Turn 02 script into a function

---
+++ code/02_count_up_counties_cities.R
@@ -0,0 +1,14 @@
+# the goal of this script is to use the dplyr package to count up the
+# number of cities and counties in a given state that have detailed
+# Apple mobility data available and then eventually save it to a csv
+
+# Vanessa Nguyen
+# September 22, 2021
+# vtnguyen27@usfca.edu
+
+# load functions
+source("code/functions/count_cities_counties_by_type.R")
+
+# test out the use of the function
+count_cities_counties_by_type(
+  input_state_file_name = "output/applemobilitytrends-2021-09-19_Florida.csv")
---
+++ code/functions/count_cities_counties_by_type.R
@@ -0,0 +1,43 @@
+# Script to read in Apple mobility data from an already subset csv file
+# to select columns including data about geography type, the region of the
+# state, and the type of transportation recorded. Then write out a csv
+# file that only has these parameters.
+
+# Vanessa Nguyen
+# vtnguyen27@usfca.edu
+# September 23, 2021
+
+# load the package "dplyr"
+library("dplyr")
+
+# create a function to create counts of cities and counties in any given
+# subsetted US state sorted by transportation type. this should also create
+# an output CSV file that is named based on the state that is subsetted.
+
+count_cities_counties_by_type <- function(input_state_file_name) {
+  # read in the subsetted state csv file
+  state_subset_data <- read.csv(input_state_file_name)
+
+  # use dplyr chains to select and filter the data. then tally the number of
+  # cities and counties with mobility data, sorted by transportation type.
+  count_state_transportation <- state_subset_data %>%
+    select(geo_type, region, transportation_type) %>%
+    group_by(geo_type, transportation_type) %>%
+    tally()
+
+  # check that the dplyr pipeline actually has data in it
+  if (nrow(count_state_transportation) < 2) {
+    stop("ERROR: Double check to make sure the input file is subsetted.")
+  }
+
+  if (nrow(count_state_transportation) > 6) {
+    stop("ERROR: Double check to make sure the input file is subsetted.")
+  }
+
+  # write out the result of the dplyr chain as a csv named based on the input
+  # file name.
+  write.csv(count_state_transportation, file = paste0("output/",
+                                             tools::file_path_sans_ext(
+                                              basename(input_state_file_name)),
+                                             "_cities_counties_count.csv"))
+}
