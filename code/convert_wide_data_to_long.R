55dff8b69e6b10d15336b7d991091f27515c4463
Vanessa Nguyen <vtnguyen27@dons.usfca.edu>
2021-09-30 02:11:16 -0700

    Commit third pass of R code to convert data to long form

---
+++ code/convert_wide_data_to_long.R
@@ -0,0 +1,28 @@
+# Script to read in Apple mobility data subsetted by state from a csv file
+# and convert the dataset to long form using tidyr package.
+
+# Vanessa Nguyen
+# vtnguyen27@usfca.edu
+# September 29, 2021
+
+library(readr)
+library(tidyr)
+
+# create a function to convert any subset US state dataset to long form
+# this should also create an output CSV file that is named based on the state
+convert_wide_data_to_long <- function(state_name) {
+  subset_state_data <- paste0("output/subset_state/applemobilitytrends-",
+                              "2021-09-21_",
+                              state_name,
+                              ".csv")
+
+  wide_to_long_state <- pivot_longer(read_csv(subset_state_data),
+    cols = 7:624,
+    names_to = "date",
+    values_to = "relative_mobility"
+  )
+
+  write_csv(wide_to_long_state, file = paste0("output/long_data/",
+         tools::file_path_sans_ext(basename(subset_state_data)),
+         "_long.csv"))
+}
