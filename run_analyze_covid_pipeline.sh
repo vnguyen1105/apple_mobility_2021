c6348ce8fcbb1228723296e034a4b5d82588d43b
Vanessa Nguyen <vtnguyen27@dons.usfca.edu>
2021-10-18 16:50:35 -0700

    Add if statement to check for parameters

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -2,9 +2,22 @@
 
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell.
+
 # This script expects two command line parameters:
 # the first should be a US state, and the second is the raw Apple mobility
 # data csv file downloaded from apple.com
+
+# Vanessa Nguyen
+# October 18, 2021
+# vtnguyen27@usfca.edu
+
+if [ $# -eq 0 ]
+then
+  echo "To run this script, supply two arguments:"
+  echo "The first is the name of the state to be analyzed."
+  echo "The second is the path to the mobility data csv file."
+  exit 1
+fi
 
 Rscript -e "rmarkdown::render('Analysis.Rmd', \
             params = list(state = '$1', \
