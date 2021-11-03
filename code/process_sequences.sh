cf16670cbf721ac56e13e1118478fddf002718ce
Vanessa Nguyen <vtnguyen27@dons.usfca.edu>
2021-11-03 09:35:05 +0000

    Add ALL parameter to script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -11,20 +11,33 @@
 # defensive programming to ensure the correct number of input files
 if [ $# -eq 0 ]
 then
-	echo "To run this script, supply a sequencing fasta.gz file."
+	echo "To run this script, supply a sequencing fasta.gz file. To execute all functions, type ALL after supplying the file."
 	exit 1
-elif [ $# -gt 1 ]
+elif [ $# -gt 2 ]
 then
-	echo "This script will only run with one sequencing file. Please ensure that only
-	one is supplied"
+	echo "This script will only run with one sequencing file. Please ensure that only one is supplied. To execute all functions, type ALL after supplying the file."
 	exit 1
 fi
 
-# Calculate the total number of sequences in the file
-{ echo "The total number of sequences in this file is: " &
-zgrep -c ">" "$1" ; } > /home/Nguyen_Vanessa/apple_mobility_2021/output/parsed_sars_cov_2_seq_using_linux.txt
-
 # Tally up the number of sequences in the dataset from each country.
-{ echo "The number of sequences by country is: " &
-zcat "$1" | zgrep ">" | cut -d"|" -f21 | sort | uniq -c | sort -rn; } >> /home/Nguyen_Vanessa/apple_mobility_2021/output/parsed_sars_cov_2_seq_using_linux.txt
+zcat "$1" | zgrep ">" | cut -d"|" -f21 | sort | uniq -c | sort -rn > /home/Nguyen_Vanessa/apple_mobility_2021/output/covid_seq_tally.txt
 
+# Calculate the total number of sequences in the file
+assArray1[TOTAL_SEQ]=$(zgrep -c ">" "$1")
+
+if [ "$2" == "ALL" ]
+then
+	{ echo "The total number of sequences in this file is: ${assArray1[TOTAL_SEQ]}"; } >> /home/Nguyen_Vanessa/apple_mobility_2021/output/covid_seq_tally.txt
+fi
+
+# Tally up the number of sequences in the dataset that have more than or less than 29000 bp,
+# which is the rough length of the full SARS-CoV-2 sequence.
+if [ "$2" == "ALL" ]
+then
+	bioawk -c fastx '{print $seq}' "$1" | awk '{print length}' | awk 'BEGIN{s = 0}; $1 < 29000 {s += 1}; END{print "# of seq < 29,000bp: "s}' >> /home/Nguyen_Vanessa/apple_mobility_2021/output/covid_seq_tally.txt
+fi
+
+if [ "$2" == "ALL" ]
+then
+	bioawk -c fastx '{print $seq}' "$1" | awk '{print length}' | awk 'BEGIN{b = 0}; $1 > 29000 {b += 1}; END{print "# of seq > 29,000bp: "b}' >> /home/Nguyen_Vanessa/apple_mobility_2021/output/covid_seq_tally.txt
+fi
