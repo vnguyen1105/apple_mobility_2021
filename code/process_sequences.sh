99ace2a53cab46d3616ab475c1b109500028f8b5
Vanessa Nguyen <vtnguyen27@dons.usfca.edu>
2021-11-22 08:47:38 +0000

    Remove redirect from bash script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -15,16 +15,19 @@ then
 	exit 1
 elif [ $# -gt 1 ]
 then
-	echo "This script will only run with one sequencing file. Please ensure that only
-	one is supplied"
+	echo "This script will only run with one sequencing file. Please ensure that only one is supplied"
 	exit 1
 fi
 
 # Calculate the total number of sequences in the file
-{ echo "The total number of sequences in this file is: " &
-zgrep -c ">" "$1" ; } > /home/Nguyen_Vanessa/apple_mobility_2021/output/parsed_sars_cov_2_seq_using_linux.txt
+echo "The total number of sequences in this file is: "
+zgrep -c ">" "$1"
 
 # Tally up the number of sequences in the dataset from each country.
-{ echo "The number of sequences by country is: " &
-zcat "$1" | zgrep ">" | cut -d"|" -f21 | sort | uniq -c | sort -rn; } >> /home/Nguyen_Vanessa/apple_mobility_2021/output/parsed_sars_cov_2_seq_using_linux.txt
+echo "The number of sequences by country is: "
+zcat "$1" | zgrep ">" | cut -d"|" -f21 | sort | uniq -c | sort -rn
 
+# Tally up the number of sequences in the dataset that have more than or less than 29000 bp,
+# which is the rough length of the full SARS-CoV-2 sequence.
+bioawk -c fastx '{print $seq}' "$1" | awk '{print length}' | awk 'BEGIN{s = 0}; $1 < 29000 {s += 1}; END{print "# of seq < 29,000bp: "s}'
+bioawk -c fastx '{print $seq}' "$1" | awk '{print length}' | awk 'BEGIN{b = 0}; $1 > 29000 {b += 1}; END{print "# of seq > 29,000bp: "b}'
